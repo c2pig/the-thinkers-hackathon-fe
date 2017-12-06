62d94f28b59c5f17bb0b6b247b63910c8b80fc6c
Kong <kwkong@seekasia.com>
2017-12-06 18:33:54 +0800

    add semantic ui + css, add sample component usage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Container, Button } from 'semantic-ui-react';
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
+            <Container>
                 <h2>Home page</h2>
-            </div>
+                <Button>
+                    Click Here
+                </Button>
+            </Container>
         )
     }
 }
