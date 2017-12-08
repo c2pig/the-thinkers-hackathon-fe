9c818e507c9c4a20bb297856057f63188953f8ee
Chew Zhen Yong <zhenyong.chew@seekasia.com>
2017-12-08 18:00:21 +0800

    create loop form draft

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Container, Button } from 'semantic-ui-react';
+import CreateLoopModal from 'components/CreateLoopModal/CreateLoopModal';
 
 class Home extends React.Component {
-    render() {
-        return (
-            <Container>
-                <h2>Home page</h2>
-                <Button>
-                    Click Here
-                </Button>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <CreateLoopModal />
+        <h2>Home page</h2>
+        <Button>Click Here</Button>
+      </Container>
+    );
+  }
 }
 
 export default Home;
