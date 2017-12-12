a401ed2e55f1142d08fec23cea515ebf42193f10
Chew Zhen Yong <zhenyong.chew@seekasia.com>
2017-12-12 14:12:34 +0800

    fix reply panel on bottom

--- src/pages/Loop/Loop.js
+++ src/pages/Loop/Loop.js
@@ -1,148 +1,245 @@
-import React from 'react'
-import { Input, Modal, Container, Message, Image, Feed, Card, Icon, Rating, Label, Comment, Form, Header, Item, Segment, Button, Divider } from 'semantic-ui-react'
+import React from 'react';
+import {
+  Input,
+  Modal,
+  Container,
+  Message,
+  Image,
+  Feed,
+  Card,
+  Icon,
+  Rating,
+  Label,
+  Comment,
+  Form,
+  Header,
+  Item,
+  Segment,
+  Button,
+  Divider,
+  Sticky
+} from 'semantic-ui-react';
 
-const Profile = ({description, profilePicture, headline}) => {
-
-  return <Item.Group><Item>
-    <Item.Image size='small' src={profilePicture} />
-    <Item.Content>
-      <Item.Header as='a'>{headline}</Item.Header>
-      <Item.Description>
-        <p>{description}</p>
-      </Item.Description>
-    </Item.Content>
-  </Item></Item.Group>
-}
+const Profile = ({ description, profilePicture, headline }) => {
+  return (
+    <Item.Group>
+      <Item>
+        <Item.Image size="small" src={profilePicture} />
+        <Item.Content>
+          <Item.Header as="a">{headline}</Item.Header>
+          <Item.Description>
+            <p>{description}</p>
+          </Item.Description>
+        </Item.Content>
+      </Item>
+    </Item.Group>
+  );
+};
 
 const TextPost = () => {
-  const text = "Hello...You are so beautiful.  May i be your friend?"
-  return  <Item.Group relaxed>
-    <Item>
-      <Item.Image size='small' src='/kong.jpg' />
-      <Item.Content verticalAlign='middle'>
-        <Item.Header>Content A</Item.Header>
-        <Item.Description>{text}</Item.Description>
-        <Item.Extra>
-          <Button floated='right'>
-            Action
-          </Button>
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+  const text = 'Hello...You are so beautiful.  May i be your friend?';
+  return (
+    <Item.Group relaxed>
+      <Item>
+        <Item.Image size="small" src="/kong.jpg" />
+        <Item.Content verticalAlign="middle">
+          <Item.Header>Content A</Item.Header>
+          <Item.Description>{text}</Item.Description>
+          <Item.Extra>
+            <Button floated="right">Action</Button>
+          </Item.Extra>
+        </Item.Content>
+      </Item>
     </Item.Group>
-}
+  );
+};
 
-const ContactMe = ({headline, description, phone, email}) => {
-  return <Card fluid>
-   <Card.Content>
-     <Card.Meta>I am talent warrior</Card.Meta>
-     <Card.Description>asdfasdfasdf</Card.Description>
-   </Card.Content>
-   <Card.Content>
-     <Icon name='mail outline' />{email}
-     <Icon name='phone' />{phone}
-   </Card.Content>
- </Card>
-}
+const ContactMe = ({ headline, description, phone, email }) => {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Card.Meta>I am talent warrior</Card.Meta>
+        <Card.Description>asdfasdfasdf</Card.Description>
+      </Card.Content>
+      <Card.Content>
+        <Icon name="mail outline" />
+        {email}
+        <Icon name="phone" />
+        {phone}
+      </Card.Content>
+    </Card>
+  );
+};
 
-const DropMessage = ({msg}) => {
-  return <Message>{msg}</Message>
-}
+const DropMessage = ({ msg }) => {
+  return <Message>{msg}</Message>;
+};
 
-const PostJD = ({jobTitle, company, tags}) => {
-  return <Card fluid>
-   <Card.Content>
-     <Card.Header>{jobTitle}</Card.Header>
-     <Card.Meta>{company}</Card.Meta>
-     {tags.map((tag,i) => {
-      return <Label key={"label-" + i}>{tag}</Label>
-     })}
-   </Card.Content>
- </Card>
-}
+const PostJD = ({ jobTitle, company, tags }) => {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Card.Header>{jobTitle}</Card.Header>
+        <Card.Meta>{company}</Card.Meta>
+        {tags.map((tag, i) => {
+          return <Label key={'label-' + i}>{tag}</Label>;
+        })}
+      </Card.Content>
+    </Card>
+  );
+};
 
-const Tags = ({tags}) => {
-  return <Label.Group >{tags.map((x,i) => <Label key={i}>{x}</Label>)}</Label.Group>
-}
+const Tags = ({ tags }) => {
+  return (
+    <Label.Group>{tags.map((x, i) => <Label key={i}>{x}</Label>)}</Label.Group>
+  );
+};
 
-const Reply = ({posts}) => {
-  return <Comment.Group size='small'>
-    {posts.map(({postType, userName, date, message, rating, totalHired, headline, phone, email, tags }, i) => {
-      return <Comment key={"comment-i"+i}>
-            <Comment.Avatar as='a' src='kong.jpg' />
-            <Comment.Content>
-              <Comment.Author as='a'>{userName}</Comment.Author>
-              <Comment.Metadata>
-                <span>{date}</span>
-              </Comment.Metadata>
-            </Comment.Content>
-            <Rating icon='star' defaultRating={rating} maxRating={5} />
-            <Label><Icon name='user' />{totalHired} Hired</Label>
-            { postType === 'contact-me' && <ContactMe headline={headline} description={message}
-              phone={phone} email={email} />
-            }
-            { postType === 'drop-message' && <DropMessage msg={message} /> }
-            { postType === 'post-jd' && <PostJD jobTitle="Graphic Designer" company="abc co" tags={tags}/> }
-            <Divider hidden />
-          </Comment>
-
-    })}
-  </Comment.Group>
-}
+const Reply = ({ posts }) => {
+  return (
+    <Comment.Group size="small">
+      {posts.map(
+        (
+          {
+            postType,
+            userName,
+            date,
+            message,
+            rating,
+            totalHired,
+            headline,
+            phone,
+            email,
+            tags
+          },
+          i
+        ) => {
+          return (
+            <Comment key={'comment-i' + i}>
+              <Comment.Avatar as="a" src="kong.jpg" />
+              <Comment.Content>
+                <Comment.Author as="a">{userName}</Comment.Author>
+                <Comment.Metadata>
+                  <span>{date}</span>
+                </Comment.Metadata>
+              </Comment.Content>
+              <Rating icon="star" defaultRating={rating} maxRating={5} />
+              <Label>
+                <Icon name="user" />
+                {totalHired} Hired
+              </Label>
+              {postType === 'contact-me' && (
+                <ContactMe
+                  headline={headline}
+                  description={message}
+                  phone={phone}
+                  email={email}
+                />
+              )}
+              {postType === 'drop-message' && <DropMessage msg={message} />}
+              {postType === 'post-jd' && (
+                <PostJD
+                  jobTitle="Graphic Designer"
+                  company="abc co"
+                  tags={tags}
+                />
+              )}
+              <Divider hidden />
+            </Comment>
+          );
+        }
+      )}
+    </Comment.Group>
+  );
+};
 
 class Loop extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = { open: false }
+    this.state = { open: false };
   }
 
-  close = () => this.setState({ open: false })
+  close = () => this.setState({ open: false });
 
   render() {
     const tags = ['Honey Massage', 'Royale Massage', 'FireWind Wheel Massage'];
     const posts = [
-      { postType: 'drop-message', userName: 'Kong', date: 'Today at 5:42PM', message: 'Hello', rating: 1, totalHired: 10, headline:'i am kong', phone:'123', email:'kong@gmail.com' },
-      { postType: 'contact-me', cuserName: 'Kong1', date: 'Today at 5:42PM', message: 'Hello', rating: 2, totalHired: 99, headline:'i am king kong', phone:'123', email:'kong1@gmail.com' },
-      { postType: 'post-jd', userName: 'Kong2', date: 'Today at 5:42PM', message: 'Hello', rating: 4, totalHired: 5, headline:'i am kong king', phone:'123', email:'kong2@gmail.com', tags:['Java', 'NodeJs', 'FrontEnd'] }
+      {
+        postType: 'drop-message',
+        userName: 'Kong',
+        date: 'Today at 5:42PM',
+        message: 'Hello',
+        rating: 1,
+        totalHired: 10,
+        headline: 'i am kong',
+        phone: '123',
+        email: 'kong@gmail.com'
+      },
+      {
+        postType: 'contact-me',
+        cuserName: 'Kong1',
+        date: 'Today at 5:42PM',
+        message: 'Hello',
+        rating: 2,
+        totalHired: 99,
+        headline: 'i am king kong',
+        phone: '123',
+        email: 'kong1@gmail.com'
+      },
+      {
+        postType: 'post-jd',
+        userName: 'Kong2',
+        date: 'Today at 5:42PM',
+        message: 'Hello',
+        rating: 4,
+        totalHired: 5,
+        headline: 'i am kong king',
+        phone: '123',
+        email: 'kong2@gmail.com',
+        tags: ['Java', 'NodeJs', 'FrontEnd']
+      }
     ];
 
-     const profileCover = {
-       description : 'Hi, I am Jeannie, a sexy and hot lady.  I offer honey massage service...',
-       profilePicture: 'jeannie.jpg',
-       headline: "Honey Massage"
-     };
+    const profileCover = {
+      description:
+        'Hi, I am Jeannie, a sexy and hot lady.  I offer honey massage service...',
+      profilePicture: 'jeannie.jpg',
+      headline: 'Honey Massage'
+    };
 
-    return <Container>
-      <Divider hidden />
-      <Segment >
-        <Label attached='top right' icon='close' />
-        <Tags tags={tags} />
-      </Segment>
-      <Segment vertical>
-        <Profile {...profileCover} />
-      </Segment>
-        <Reply posts={posts} />
-      <Segment vertical textAlign='center'>
-        <Modal trigger={<Button fluid content='Reply' icon='reply' />} closeIcon>
-
-            <Modal.Content>
-            <Segment textAlign='center'>
-              <Input action={{ color: 'teal', icon: 'arrow right' }} />
-              <Divider center />
-              <Button.Group >
-                <Button>Attach a Job</Button>
-                <Button.Or />
-                <Button positive>Contact Me</Button>
-              </Button.Group>
-            </Segment>
-            </Modal.Content>
-          </Modal>
-        <Divider hidden />
-      </Segment>
-      </Container>
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <Container
+          style={{ overflowY: 'auto', flexBasis: 'calc(90vh - 140px)' }}
+        >
+          <Divider hidden />
+          <Segment>
+            <Label attached="top right" icon="close" />
+            <Tags tags={tags} />
+          </Segment>
+          <Segment vertical>
+            <Profile {...profileCover} />
+          </Segment>
+          <Reply posts={posts} />
+        </Container>
+        <Container style={{ flex: '0 0 auto' }}>
+          <Segment vertical textAlign="center">
+            <Input
+              fluid
+              action={{ color: 'teal', icon: 'arrow right' }}
+              placeholder="leave a message"
+            />
+            <Divider horizontal>Or</Divider>
+            <Button.Group fluid>
+              <Button>Attach a Job</Button>
+              {/* <Button.Or /> */}
+              <Button positive>Left my contact</Button>
+            </Button.Group>
+          </Segment>
+        </Container>
+      </div>
+    );
   }
 }
-
 
 export default Loop;
