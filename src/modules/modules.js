63f86b28c9f14fb2290ba3d59f82173ebae0262c
Chew Zhen Yong <zhenyong.chew@seekasia.com>
2017-12-12 12:50:50 +0800

    add login form with redux-form and user store

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -1,19 +1,24 @@
 import { routerReducer } from 'react-router-redux';
 import { persistCombineReducers } from 'redux-persist';
 import localForage from 'localforage';
+import { reducer as formReducer } from 'redux-form';
 
 import loops, * as fromLoops from './loops';
+import userReducer from './user';
 
 const persistConfig = {
-    key: 'root',
-    storage: localForage,
+  key: 'root',
+  storage: localForage
 };
 
 export default persistCombineReducers(persistConfig, {
-    routing: routerReducer,
-    loops,
+  routing: routerReducer,
+  form: formReducer,
+  user: userReducer,
+  loops
 });
 
 // Selector
-export const getLoopsSearchKeywords = state => fromLoops.getSearchKeywords(state.loops);
+export const getLoopsSearchKeywords = state =>
+  fromLoops.getSearchKeywords(state.loops);
 export const getLoopsData = state => fromLoops.getData(state.loops);
