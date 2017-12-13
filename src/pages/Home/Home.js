e8c436bdc13323053f652e9d1f33477d76d671a6
c2pig <kokboon@gmail.com>
2017-12-13 21:42:43 +0800

    larger vote icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -99,8 +99,9 @@ class Home extends React.Component {
   VoteTopic = ({children, rating, id}) => {
     return <Grid>
     <Grid.Column width='1'>
-      <Grid.Row><Icon name='chevron up' onClick={() => {this.whenUpVote({rating, id})}}/></Grid.Row>
-      <Grid.Row><Icon name='chevron down' onClick={() => {this.whenDownVote({rating, id})}}/></Grid.Row>
+      <Grid.Row verticalAlign='top'><Icon size='large' name='chevron up' onClick={() => {this.whenUpVote({rating, id})}}/></Grid.Row>
+      <div />
+      <Grid.Row verticalAlign='bottom'><Icon size='large' name='chevron down' onClick={() => {this.whenDownVote({rating, id})}}/></Grid.Row>
       <Divider hidden />
       <Grid.Row>
       <Statistic size='mini' floated='left'>
