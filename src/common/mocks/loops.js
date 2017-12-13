7ae634a92a8649c1e358f90c882aa238a8b58118
Vivien Wee <duduvien@Wees-MacBook-Pro.local>
2017-12-14 04:49:12 +0800

    - Added new styling - Refinement

--- src/common/mocks/loops.js
+++ src/common/mocks/loops.js
@@ -7,51 +7,335 @@ export default {
     description:
       'I browsed through their career website and I’m not sure if any of the jobs posted there are for fresh grads. Was hoping someone could direct me to applying to eh appropriate job or even tell me if FreeScale are even currently hiring fresh grads?',
     tags: ['EE', 'semiconductor', 'FreeScale'],
-    comments: [],
-    rating:11
+    comments: [
+      {
+        postType: 'drop-message',
+        date: new Date('2017-12-10').toString(),
+        totalHired: 30,
+        tags: [
+          [
+            { tag: 'EE', rating: 27 },
+            { tag: 'C', rating: 82 },
+            { tag: 'design', rating: 56 },
+          ],
+        ],
+        loopId: '1',
+        message:
+          'I did my industrial training back then when it was known as Motorola. Since I was working mainly with the process engineers, the hours were not that long but if there is any problems with the line they had to return at any time of the day. Not so sure though if they are hiring any fresh grads at the moment.      ',
+        username: 'kent',
+      },
+      {
+        postType: 'post-jd',
+        date: new Date('2017-12-11').toString(),
+        totalHired: 245,
+        tags: [
+          [
+            { tag: 'hr', rating: 83 },
+            { tag: 'recruiter', rating: 152 },
+            { tag: 'mmu', rating: 92 },
+          ],
+        ],
+        message: 'Refer below!',
+        loopId: '1',
+        job: {
+          jobTitle: 'Fresh Graduate - EE',
+          company: 'FreeScale',
+          tags: ['passion', 'C', 'machine language'],
+          yearsOfExperience: 'Fresh Graduate',
+          location: 'Kuala Lumpur',
+          description: `<h3>The Role</h3>
+          <p>The FreeScale are committed to helping people live more fulfilling and productive working lives and helping organisations succeed. To delivery on this promise, we are investing in increasing our global product development capability in our International division, with a small, high caliber product development team charged with solving difficult problems and bringing big ideas to life.</p>
+          <p>As a Electrical Engineer at FreeScale, you will specialize on creating a great user experience and solving user challenges through developing novel user interface designs and prototypes, in a team following agile principles.</p>
+          <h3>Key Responsibilities</h3>
+          <ul>
+          <li>
+          <p>Play a key role in transforming product concepts into next-generation web applications that would benefit millions of candidates in finding their dream job.</p>
+          </li>
+          <li>
+          <p>Work with Product Owners to produce fully functional front-end prototype that users can validate.</p>
+          </li>
+          <li>
+          <p>Apply visual styles and interactions through various emerging technologies, such as ReactJS, ES6 and PostCSS.</p>
+          </li>
+          <li>
+          <p>Understand, critique and help improve basic interaction models and wireframes.</p>
+          </li>
+          <li>
+          <p>Focus on client-side development in a continuous delivery environment and engage with back- end engineers in delivering more differentiated, robust and highly tested products.</p>
+          </li>
+          <li>
+          <p>Build responsive applications that work perfectly in web, tablet and mobile platforms.</p>
+          </li>
+          <li>
+          <p>Work in an environment where quality is at the center of every decision and where every team member is responsible for every part of the process.</p>
+          </li>
+          </ul>
+          <h3>Essential Skills, Experience and Competencies</h3>
+          <ul>
+          <li>
+          <p>Creative and passionate in delivering insanely great products, with a good understanding of accessibility and usability.</p>
+          </li>
+          <li>
+          <p>Drive to excel in front-end development and advancing knowledge in front-end technologies.</p>
+          </li>
+          <li>
+          <p>Good understanding and relevant experience in HTML5, CSS3 and JavaScript is critical.</p>
+          </li>
+          <li>
+          <p>Experience with unit testing, TDD and continuous delivery is highly regarded as delivering quality products is critical to us.</p>
+          </li>
+          <li>
+          <p>Knowledge and experience in front-end frameworks such as ReactJS or AngularJS is an added advantage.</p>
+          </li>
+          <li>
+          <p>Practice around various Agile methodologies in the workplace is advantageous.</p>
+          </li>
+          </ul>`,
+        },
+        username: 'wawa',
+      },
+    ],
+    rating: 11,
   },
   '2': {
-    id: 2,
-    username: 'jeannie',
+    id: '2',
+    username: 'felicia',
     status: 'open',
     topic: '1 year+  experience in HR payroll interested in recruitment',
     description:
       'I graduated on May 2015 and started my career as HR payroll with Hewitt. I’ve been with them close to two years and recently got the chance to go into recruitment. I realize I’m more passionate in this area and would like to ask around if there’s any opportunities anywhere that I can explore further.',
-    tags: ['massage', 'back', 'shoulder', '5years'],
-    comments: [],
-    rating: 33
+    tags: ['hr', 'payroll', 'recruitment', 'Hewitt'],
+    comments: [
+      {
+        postType: 'contact-me',
+        date: new Date('2017-12-12').toString(),
+        totalHired: 30,
+        tags: [
+          [
+            { tag: 'hr', rating: 83 },
+            { tag: 'recruiter', rating: 152 },
+            { tag: 'mmu', rating: 92 },
+          ],
+        ],
+        message: 'Ring me up! I have a few companies looking for recruiters.',
+        contact: {
+          fullname: 'Nurul Wahida',
+          phone: '018-8993346',
+          email: 'nurulwawa@hewitt.com',
+          position: 'Senior HR',
+          company: 'Hewitt Co.',
+        },
+        loopId: '2',
+        username: 'wawa',
+      },
+      {
+        postType: 'drop-message',
+        date: new Date('2017-12-10').toString(),
+        totalHired: 0,
+        tags: [
+          [
+            { tag: 'project mgmt', rating: 145 },
+            { tag: 'people skill', rating: 57 },
+            { tag: 'general programming', rating: 157 },
+          ],
+        ],
+        loopId: '2',
+        message:
+          'I have a friend whose company may have such openings too..but they may only need freelancer.',
+        username: 'jerome',
+      },
+      {
+        postType: 'post-jd',
+        date: new Date('2017-12-11').toString(),
+        totalHired: 4,
+        tags: [
+          [
+            { tag: 'Java', rating: 98 },
+            { tag: 'freelance', rating: 57 },
+            { tag: 'nodeJS', rating: 43 },
+            { tag: 'JavaScript', rating: 99 },
+          ],
+        ],
+        message: 'This may suit you!',
+        loopId: 1,
+        job: {
+          jobTitle: 'HR Payroll',
+          company: 'Ambitions Inc.',
+          tags: ['payroll', 'hr', 'english'],
+          yearsOfExperience: 'Min. 1 year of experience',
+          location: 'Selangor',
+          description: `<h3>The Role</h3>
+        <p>The Ambitions Inc are committed to helping people live more fulfilling and productive working lives and helping organisations succeed. To delivery on this promise, we are investing in increasing our global product development capability in our International division, with a small, high caliber product development team charged with solving difficult problems and bringing big ideas to life.</p>
+        <p>As a HR Payroll at Ambitions Inc, you will specialize on creating a great user experience and solving user challenges through developing novel user interface designs and prototypes, in a team following agile principles.</p>
+        <h3>Key Responsibilities</h3>
+        <ul>
+        <li>
+        <p>Play a key role in transforming product concepts into next-generation web applications that would benefit millions of candidates in finding their dream job.</p>
+        </li>
+        <li>
+        <p>Work with Product Owners to produce fully functional front-end prototype that users can validate.</p>
+        </li>
+        <li>
+        <p>Apply visual styles and interactions through various emerging technologies, such as ReactJS, ES6 and PostCSS.</p>
+        </li>
+        <li>
+        <p>Understand, critique and help improve basic interaction models and wireframes.</p>
+        </li>
+        <li>
+        <p>Focus on client-side development in a continuous delivery environment and engage with back- end engineers in delivering more differentiated, robust and highly tested products.</p>
+        </li>
+        <li>
+        <p>Build responsive applications that work perfectly in web, tablet and mobile platforms.</p>
+        </li>
+        <li>
+        <p>Work in an environment where quality is at the center of every decision and where every team member is responsible for every part of the process.</p>
+        </li>
+        </ul>
+        <h3>Essential Skills, Experience and Competencies</h3>
+        <ul>
+        <li>
+        <p>Creative and passionate in delivering insanely great products, with a good understanding of accessibility and usability.</p>
+        </li>
+        <li>
+        <p>Drive to excel in front-end development and advancing knowledge in front-end technologies.</p>
+        </li>
+        <li>
+        <p>Good understanding and relevant experience in HTML5, CSS3 and JavaScript is critical.</p>
+        </li>
+        <li>
+        <p>Experience with unit testing, TDD and continuous delivery is highly regarded as delivering quality products is critical to us.</p>
+        </li>
+        <li>
+        <p>Knowledge and experience in front-end frameworks such as ReactJS or AngularJS is an added advantage.</p>
+        </li>
+        <li>
+        <p>Practice around various Agile methodologies in the workplace is advantageous.</p>
+        </li>
+        </ul>`,
+        },
+        username: 'kong',
+      },
+      {
+        postType: 'drop-message',
+        date: new Date('2017-12-10').toString(),
+        totalHired: 30,
+        tags: [
+          [
+            { tag: 'design', rating: 83 },
+            { tag: 'ux', rating: 152 },
+            { tag: 'mmu', rating: 92 },
+          ],
+        ],
+        loopId: '2',
+        message:
+          'Are you willing to take a paycut? Recuriter’s life aint easier!',
+        username: 'jeannie',
+      },
+    ],
+    rating: 23,
   },
   '3': {
-    id: 3,
+    id: '3',
     username: 'jeannie',
     status: 'open',
-    topic: 'Digital/Email marketing expert, have 10 years of agency experience',
+    topic:
+      'Flexible working environment with free lunch daily! In the hunt for awesome product managers!',
     description:
-      'I have strong background and knowledge in email marketing/digital marketing. My strength is in searching for leads and conducting campaigns. Have been with several big companies in the industry like Lippincott, Braund Union, Future brands etc',
-    tags: ['drain', 'cleaner', 'expert'],
-    comments: [],
-    rating: 61
+      "Money is not the bottom line—people are. Our product managers take the time to understand customer needs and do what's necessary to delight them with our products. Work with engineering, design, marketing, and leadership to define and shape products that millions will use.",
+    tags: ['product', 'flexible working hour', 'free lunch'],
+    comments: [
+      {
+        postType: 'contact-me',
+        date: new Date('2017-12-10').toString(),
+        totalHired: 4,
+        tags: [
+          [
+            { tag: 'Java', rating: 98 },
+            { tag: 'freelance', rating: 57 },
+            { tag: 'nodeJS', rating: 43 },
+            { tag: 'JavaScript', rating: 99 },
+          ],
+        ],
+        message: 'I have a few contacts call me!',        
+        contact: {
+          fullname: 'Kong Ka Weng',
+          phone: '018-12345678',
+          email: 'kaweng.kong@gdp.com',
+          position: 'Full-Stack Engineer',
+          company: 'GDD Company',
+        },
+        loopId: '3',
+        username: 'kong',
+      },
+    ],
+    rating: 61,
   },
   '4': {
-    id: 4,
+    id: '4',
     username: 'kong',
     status: 'open',
     topic: 'Junior Accountant offering freelance accounting service',
     description:
       'I work as junior accounting exec in one of the big 4 firms. I’m handle receipts payments, sales and trade debtors, purchases and trade creditors, prepayment and accruals, assets and depreciation, journal entries. Contact me if interested.',
-    tags: ['drain', 'cleaner', 'expert'],
-    comments: [],
-    rating: 13
+    tags: ['accountant', 'audit', 'acca', 'freelance'],
+    comments: [
+      {
+        postType: 'contact-me',
+        date: new Date('2017-12-10').toString(),
+        totalHired: 4,
+        tags: [
+          [
+            { tag: 'Java', rating: 98 },
+            { tag: 'freelance', rating: 57 },
+            { tag: 'nodeJS', rating: 43 },
+            { tag: 'JavaScript', rating: 99 },
+          ],
+        ],
+        message: 'I have a few contacts call me!',        
+        contact: {
+          fullname: 'Kong Ka Weng',
+          phone: '018-12345678',
+          email: 'kaweng.kong@gdp.com',
+          position: 'Full-Stack Engineer',
+          company: 'GDD Company',
+        },
+        loopId: '3',
+        username: 'kong',
+      },
+    ],
+    rating: 13,
   },
   '5': {
-    id: 5,
+    id: '5',
     username: 'kong',
     status: 'open',
     topic: 'Experienced Front end engineer practicing Agile method',
     description:
       'My forte is in ReactJS and I have experience in HTML 5, CSS 3, and Javascript. I’ve been with Amazon for 2 years now.',
-    tags: ['drain', 'cleaner', 'expert'],
-    comments: [],
-    rating: 9
-  }
+    tags: ['Javascript', 'reactjs', 'html5', 'software engineer'],
+    comments: [{
+      postType: 'contact-me',
+      date: new Date('2017-12-11').toString(),
+      totalHired: 4,
+      tags: [
+        [
+          { tag: 'Java', rating: 98 },
+          { tag: 'freelance', rating: 57 },
+          { tag: 'nodeJS', rating: 43 },
+          { tag: 'JavaScript', rating: 99 },
+        ],
+      ],
+      message: 'I have a few contacts call me!',      
+      contact: {
+        fullname: 'Kong Ka Weng',
+        phone: '018-12345678',
+        email: 'kaweng.kong@gdp.com',
+        position: 'Full-Stack Engineer',
+        company: 'GDD Company',
+      },
+      loopId: '3',
+      username: 'kong',
+    },],
+    rating: 9,
+  },
 };
--- src/common/mocks/tags.js
+++ src/common/mocks/tags.js
@@ -3,13 +3,13 @@ export default [
     userName: 'kong',
     tags: [
       { tag: 'Java', rating: 98 },
-      { tag: 'C++', rating: 57 },
+      { tag: 'freelance', rating: 57 },
       { tag: 'nodeJS', rating: 43 },
       { tag: 'JavaScript', rating: 99 },
     ]
   },
   {
-    userName: 'kaweng',
+    userName: 'jerome',
     tags: [
       { tag: 'project mgmt', rating: 145 },
       { tag: 'people skill', rating: 57 },
@@ -17,7 +17,7 @@ export default [
     ]
   },
   {
-    userName: 'vivien',
+    userName: 'kokboon',
     tags: [
       { tag: 'performning arts', rating: 283 },
       { tag: 'yoga', rating: 152 },
@@ -33,11 +33,20 @@ export default [
     ]
   },
   {
-    userName: 'zhenyong',
+    userName: 'wawa',
     tags: [
-      { tag: 'react', rating: 191 },
-      { tag: 'product mgmt', rating: 82 },
+      { tag: 'hr', rating: 83 },
+      { tag: 'recruitment', rating: 152 },
+      { tag: 'mmu', rating: 92 },
+    ]
+  },
+  {
+    userName: 'kent',
+    tags: [
+      { tag: 'EE', rating: 127 },
+      { tag: 'C', rating: 82 },
       { tag: 'design', rating: 56 },
     ]
-  }
+  },
+
 ];
