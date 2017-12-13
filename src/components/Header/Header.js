758aad2725c30098c161e3ab8c039fc65e7f9ec7
Vivien Wee <duduvien@Wees-MacBook-Pro.local>
2017-12-13 14:20:20 +0800

    WIP

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Menu, Icon, Image } from 'semantic-ui-react';
 
-class Header extends React.Component {
+import styles from './Header.css';
+
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeItem: '',
+    };
+  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
+
     return (
-      <div>
-        <h1>Header</h1>
-      </div>
+      <Menu
+        style={{ height: '4rem', backgroundColor: '#243862', color: 'white' }}
+      >
+        <Menu.Item header style={{ color: 'inherit' }}>
+          <Image src="/LogoLoop_White.png" alt="logo" size="tiny" avatar />
+        </Menu.Item>
+        <Menu.Item
+          className={styles.menuItem}
+          name="profile"
+          active={activeItem === 'profile'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          className={styles.menuItem}
+          name="logout"
+          active={activeItem === 'logout'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position="right">
+          <Menu.Item header style={{ color: 'inherit' }}>
+            <Icon name="hand peace" size="large" />Hi, Jeannie
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     );
   }
 }
 
-export default Header;
+// export default withRouter(connect(
+//   state => {
+//     currentUserName: get
+//   }
+// )(Header));
--- src/modules/modules.js
+++ src/modules/modules.js
@@ -34,3 +34,5 @@ export const getLoopListData = state => {
 };
 
 export const getUsersData = state => fromUsers.getData(state.users);
+
+// export const getData = state => state.data;
