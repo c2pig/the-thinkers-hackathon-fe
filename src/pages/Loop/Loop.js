e42f0937f91652061c644e2fd4e93736ce142a64
c2pig <kokboon@gmail.com>
2017-12-13 17:17:21 +0800

    likes WIP

--- src/pages/Loop/Loop.js
+++ src/pages/Loop/Loop.js
@@ -18,7 +18,7 @@ import { connect } from 'react-redux';
 import mockComments from 'common/mocks/comments';
 import { Link } from 'react-router-dom';
 import { STATUS_OPEN } from 'store/loops';
-import { getHighestRatingTagName, getRelatedTag } from 'common/helpers';
+import UserComment from 'components/UserComment/UserComment';
 
 const Topic = ({ description, tags, headline }) => {
   return (
@@ -38,118 +38,20 @@ const Topic = ({ description, tags, headline }) => {
   );
 };
 
-const ContactMe = ({ headline, description, phone, email }) => {
-  return (
-    <Card fluid>
-      <Card.Content>
-        <Card.Meta>I am talent warrior</Card.Meta>
-        <Card.Description>asdfasdfasdf</Card.Description>
-      </Card.Content>
-      <Card.Content>
-        <Icon name="mail outline" />
-        {email}
-        <Icon name="phone" />
-        {phone}
-      </Card.Content>
-    </Card>
-  );
-};
-
-const DropMessage = ({ msg }) => {
-  return <Message>{msg}</Message>;
-};
-
-const PostJD = ({ jobTitle, company, tags }) => {
-  return (
-    <Card fluid>
-      <Card.Content>
-        <Card.Header>{jobTitle}</Card.Header>
-        <Card.Meta>{company}</Card.Meta>
-        {tags.map(({ tag }, i) => {
-          return <Label key={'label-' + i}>{tag}</Label>;
-        })}
-      </Card.Content>
-    </Card>
-  );
-};
-
 const Tags = ({ tags }) => {
   return (
     <Label.Group>{tags.map((x, i) => <Label key={i}>{x}</Label>)}</Label.Group>
   );
 };
 
-const UserComment = ({ comments, topicTags }) => {
-  return (
-    <Comment.Group size="small">
-      {comments.map(
-        (
-          {
-            postType,
-            username,
-            date,
-            message,
-            totalHired,
-            headline,
-            phone,
-            email,
-            tags
-          },
-          i
-        ) => {
-          const { tag, rating } = getHighestRatingTagName(
-            getRelatedTag(...tags, topicTags),
-            topicTags
-          );
-          return (
-            <Comment key={'comment-i' + i}>
-              <Comment.Avatar as="a" src={`/${username}.jpg`} />
-              <Comment.Content>
-                <Comment.Author as="a">{username}</Comment.Author>
-                <Comment.Metadata>
-                  <span>{date}</span>
-                </Comment.Metadata>
-              </Comment.Content>
-              {tag && (
-                <Label>
-                  {tag}
-                  <Label.Detail>{rating}</Label.Detail>
-                </Label>
-              )}
-              <Label>
-                <Icon name="user" />
-                {totalHired} Hired
-              </Label>
-              <Icon name="thumbs outline up" />
-              {postType === 'contact-me' && (
-                <ContactMe
-                  headline={headline}
-                  description={message}
-                  phone={phone}
-                  email={email}
-                />
-              )}
-              {postType === 'drop-message' && <DropMessage msg={message} />}
-              {postType === 'post-jd' && (
-                <PostJD
-                  jobTitle="Graphic Designer"
-                  company="abc co"
-                  tags={tags}
-                />
-              )}
-              <Divider hidden />
-            </Comment>
-          );
-        }
-      )}
-    </Comment.Group>
-  );
-};
-
 class Loop extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { open: false };
+    this.state = {
+      open: false,
+      likes: []
+     };
   }
 
   close = () => this.setState({ open: false });
@@ -207,7 +109,7 @@ class Loop extends React.Component {
           <Segment vertical>
             <Topic {...topic} />
           </Segment>
-          <UserComment comments={comments} topicTags={topic.tags} />
+          <UserComment comments={comments} topicTags={topic.tags} parentState={this.state}/>
         </Container>
         <Container>
           <ReplyPanel loopId={this.props.loop.id} />
