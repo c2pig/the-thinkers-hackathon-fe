b706a7d7bfe625085983e6b9d6423848b2bc71fc
c2pig <kokboon@gmail.com>
2017-12-13 08:56:06 +0800

    add get highest tag rating func

--- src/common/helpers/index.js
+++ src/common/helpers/index.js
@@ -7,3 +7,19 @@ export const extractTagsByUserName = (name) => {
         return tags;
     })
 }
+
+
+/* NOTE: usage - const { tag } = getHighestRatingTagName([tag:"a", rating:10]); */
+export const getHighestRatingTagName = (tags) => {
+  return tags.reduce((acc, tag) => {
+
+    if(Object.keys(acc) === 0) {
+      return tag;
+    }
+
+    if(tag.rating > acc.rating) {
+      return tag;
+    }
+
+  }, {});
+}
