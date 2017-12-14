2d75c6cb6ba0047bdea3e482374f65fcec48cf81
Chew Zhen Yong <zhenyong.chew@seekasia.com>
2017-12-14 12:59:35 +0800

    fix typo

--- src/common/mocks/users.js
+++ src/common/mocks/users.js
@@ -1,5 +1,3 @@
-
-
 export default {
   jeannie: {
     avatar: '/jeannie.jpg',
@@ -11,15 +9,11 @@ export default {
     company: 'AXD Company',
     yearsOfExperience: 3,
     location: 'Setia Alam, Shah Alam, Selangor, Malaysia.',
-    skills: [
-      'javascript',
-      'react',
-      'node js',
-    ],
+    skills: ['javascript', 'react', 'node js'],
     quote: 'Coding is my life. I eat, live, and breathe codes.',
     rank: {
       title: 'IT Apprentice',
-      votesCount: 10,
+      votesCount: 10
     },
     peopleHired: 12,
     events: [
@@ -30,7 +24,7 @@ export default {
         company: 'AXD Company',
         desc:
           'Completed frontend project and currently, working on new project which involve machine learning.',
-        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition'],
+        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition']
       },
       {
         type: 'achievement',
@@ -40,7 +34,7 @@ export default {
         image: '/grab.jpg',
         desc:
           'A hackathon organized to unearth new solutions and ideas for traffic problems and road safety.',
-        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack'],
+        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack']
       },
       {
         type: 'certificate',
@@ -48,7 +42,7 @@ export default {
         company: 'Certification of Project Management',
         position: 'CPM 2016',
         desc: 'Certification that involves testing on managing project skills.',
-        tags: ['project management', 'CPM 2016'],
+        tags: ['project management', 'CPM 2016']
       },
       {
         type: 'work',
@@ -56,7 +50,7 @@ export default {
         position: 'Full-Stack Software Engineer',
         company: 'Shoopper',
         desc: 'Building up e-commerce website.',
-        tags: ['PHP', 'Laravel', 'SQL'],
+        tags: ['PHP', 'Laravel', 'SQL']
       },
       {
         type: 'certificate',
@@ -66,7 +60,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on Ethical hacking such as corporate espionage, writing virus codes, exploit writing, and reverse engineering. You will understand advanced network packet analysis, securing IIS and Apache web servers, Windows system administration using PowerShell, Hacking SQL & Oracle database.',
-        tags: ['security', 'CEH'],
+        tags: ['security', 'CEH']
       },
       {
         type: 'education',
@@ -75,9 +69,9 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
   },
   felicia: {
     avatar: '/felicia.jpg',
@@ -89,14 +83,10 @@ export default {
     company: 'HR Sdn Bhd',
     yearsOfExperience: 1,
     location: 'Kajang, Selangor, Malaysia.',
-    skills: [
-      'payroll',
-      'recruitment',
-      'strong communication',
-    ],
+    skills: ['payroll', 'recruitment', 'strong communication'],
     rank: {
       title: 'Newbie',
-      votesCount: 3,
+      votesCount: 3
     },
     peopleHired: 2,
     events: [
@@ -107,7 +97,7 @@ export default {
         company: 'HR Sdn Bhd',
         desc:
           'Completed frontend project and currently, working on new project which involve machine learning.',
-        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition'],
+        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition']
       },
       {
         type: 'certificate',
@@ -117,7 +107,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on human resource skills and knowledge.',
-        tags: ['eCornell', 'HRCertification2015'],
+        tags: ['eCornell', 'HRCertification2015']
       },
       {
         type: 'education',
@@ -126,9 +116,9 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
   },
   kong: {
     avatar: '/kong.jpg',
@@ -140,14 +130,10 @@ export default {
     company: 'GDD Company',
     yearsOfExperience: 3,
     location: 'TTDI, Kuala Lumpur, Malaysia.',
-    skills: [
-      'javascript',
-      'react',
-      'node js',
-    ],
+    skills: ['javascript', 'react', 'node js'],
     rank: {
       title: 'IT Maniac',
-      votesCount: 104,
+      votesCount: 104
     },
     peopleHired: 4,
     events: [
@@ -158,7 +144,7 @@ export default {
         company: 'AXD Company',
         desc:
           'Completed frontend project and currently, working on new project which involve machine learning.',
-        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition'],
+        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition']
       },
       {
         type: 'achievement',
@@ -168,7 +154,7 @@ export default {
         image: '/grab.jpg',
         desc:
           'A hackathon organized to unearth new solutions and ideas for traffic problems and road safety.',
-        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack'],
+        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack']
       },
       {
         type: 'certificate',
@@ -176,7 +162,7 @@ export default {
         company: 'Certification of Project Management',
         position: 'CPM 2016',
         desc: 'Certification that involves testing on managing project skills.',
-        tags: ['project management', 'CPM 2016'],
+        tags: ['project management', 'CPM 2016']
       },
       {
         type: 'work',
@@ -184,7 +170,7 @@ export default {
         position: 'Full-Stack Software Engineer',
         company: 'Shoopper',
         desc: 'Building up e-commerce website.',
-        tags: ['PHP', 'Laravel', 'SQL'],
+        tags: ['PHP', 'Laravel', 'SQL']
       },
       {
         type: 'certificate',
@@ -194,7 +180,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on Ethical hacking such as corporate espionage, writing virus codes, exploit writing, and reverse engineering. You will understand advanced network packet analysis, securing IIS and Apache web servers, Windows system administration using PowerShell, Hacking SQL & Oracle database.',
-        tags: ['security', 'CEH'],
+        tags: ['security', 'CEH']
       },
       {
         type: 'education',
@@ -203,9 +189,9 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
   },
   jerome: {
     avatar: '/jerome.jpg',
@@ -217,14 +203,10 @@ export default {
     company: 'Global Delivery Pod',
     yearsOfExperience: 3,
     location: 'Petaling Jaya, Selangor, Malaysia.',
-    skills: [
-      'problem-solving',
-      'time-management',
-      'decision-making',
-    ],
+    skills: ['problem-solving', 'time-management', 'decision-making'],
     rank: {
       title: 'Newbie',
-      votesCount: 2,
+      votesCount: 2
     },
     peopleHired: 0,
     events: [
@@ -235,7 +217,7 @@ export default {
         company: 'AXD Company',
         desc:
           'Completed frontend project and currently, working on new project which involve machine learning.',
-        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition'],
+        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition']
       },
       {
         type: 'achievement',
@@ -245,7 +227,7 @@ export default {
         image: '/grab.jpg',
         desc:
           'A hackathon organized to unearth new solutions and ideas for traffic problems and road safety.',
-        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack'],
+        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack']
       },
       {
         type: 'certificate',
@@ -253,7 +235,7 @@ export default {
         company: 'Certification of Project Management',
         position: 'CPM 2016',
         desc: 'Certification that involves testing on managing project skills.',
-        tags: ['project management', 'CPM 2016'],
+        tags: ['project management', 'CPM 2016']
       },
       {
         type: 'work',
@@ -261,7 +243,7 @@ export default {
         position: 'Full-Stack Software Engineer',
         company: 'Shoopper',
         desc: 'Building up e-commerce website.',
-        tags: ['PHP', 'Laravel', 'SQL'],
+        tags: ['PHP', 'Laravel', 'SQL']
       },
       {
         type: 'certificate',
@@ -271,7 +253,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on Ethical hacking such as corporate espionage, writing virus codes, exploit writing, and reverse engineering. You will understand advanced network packet analysis, securing IIS and Apache web servers, Windows system administration using PowerShell, Hacking SQL & Oracle database.',
-        tags: ['security', 'CEH'],
+        tags: ['security', 'CEH']
       },
       {
         type: 'education',
@@ -280,9 +262,9 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
   },
   wawa: {
     avatar: '/wawa.jpg',
@@ -298,12 +280,12 @@ export default {
       'strong communication',
       'planning',
       'hunter’s mentality',
-      'listening',
+      'listening'
     ],
     quote: 'Listen to people is what I always do before I put my words out.',
     rank: {
-      title: 'Recruiter Maniac',
-      votesCount: 271,
+      title: 'HR Maniac',
+      votesCount: 271
     },
     peopleHired: 245,
     events: [
@@ -314,7 +296,7 @@ export default {
         company: 'AXD Company',
         desc:
           'Completed frontend project and currently, working on new project which involve machine learning.',
-        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition'],
+        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition']
       },
       {
         type: 'achievement',
@@ -324,7 +306,7 @@ export default {
         image: '/grab.jpg',
         desc:
           'A hackathon organized to unearth new solutions and ideas for traffic problems and road safety.',
-        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack'],
+        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack']
       },
       {
         type: 'certificate',
@@ -332,7 +314,7 @@ export default {
         company: 'Certification of Project Management',
         position: 'CPM 2016',
         desc: 'Certification that involves testing on managing project skills.',
-        tags: ['project management', 'CPM 2016'],
+        tags: ['project management', 'CPM 2016']
       },
       {
         type: 'work',
@@ -340,7 +322,7 @@ export default {
         position: 'Full-Stack Software Engineer',
         company: 'Shoopper',
         desc: 'Building up e-commerce website.',
-        tags: ['PHP', 'Laravel', 'SQL'],
+        tags: ['PHP', 'Laravel', 'SQL']
       },
       {
         type: 'certificate',
@@ -350,7 +332,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on Ethical hacking such as corporate espionage, writing virus codes, exploit writing, and reverse engineering. You will understand advanced network packet analysis, securing IIS and Apache web servers, Windows system administration using PowerShell, Hacking SQL & Oracle database.',
-        tags: ['security', 'CEH'],
+        tags: ['security', 'CEH']
       },
       {
         type: 'education',
@@ -359,9 +341,9 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
   },
   kent: {
     avatar: '/kent.jpg',
@@ -377,11 +359,11 @@ export default {
       'strong communication',
       'planning',
       'hunter’s mentality',
-      'listening',
+      'listening'
     ],
     rank: {
       title: 'EE Know-It-All',
-      votesCount: 87,
+      votesCount: 87
     },
     peopleHired: 30,
     events: [
@@ -392,7 +374,7 @@ export default {
         company: 'AXD Company',
         desc:
           'Completed frontend project and currently, working on new project which involve machine learning.',
-        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition'],
+        tags: ['machine learning', 'react', 'AWS', 'IT', 'voice recognition']
       },
       {
         type: 'achievement',
@@ -402,7 +384,7 @@ export default {
         image: '/grab.jpg',
         desc:
           'A hackathon organized to unearth new solutions and ideas for traffic problems and road safety.',
-        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack'],
+        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack']
       },
       {
         type: 'certificate',
@@ -410,7 +392,7 @@ export default {
         company: 'Certification of Project Management',
         position: 'CPM 2016',
         desc: 'Certification that involves testing on managing project skills.',
-        tags: ['project management', 'CPM 2016'],
+        tags: ['project management', 'CPM 2016']
       },
       {
         type: 'work',
@@ -418,7 +400,7 @@ export default {
         position: 'Full-Stack Software Engineer',
         company: 'Shoopper',
         desc: 'Building up e-commerce website.',
-        tags: ['PHP', 'Laravel', 'SQL'],
+        tags: ['PHP', 'Laravel', 'SQL']
       },
       {
         type: 'certificate',
@@ -428,7 +410,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on Ethical hacking such as corporate espionage, writing virus codes, exploit writing, and reverse engineering. You will understand advanced network packet analysis, securing IIS and Apache web servers, Windows system administration using PowerShell, Hacking SQL & Oracle database.',
-        tags: ['security', 'CEH'],
+        tags: ['security', 'CEH']
       },
       {
         type: 'education',
@@ -437,9 +419,9 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
   },
   kokboon: {
     avatar: '/kokboon.jpg',
@@ -451,14 +433,10 @@ export default {
     company: 'Multimedia University',
     yearsOfExperience: 0,
     location: 'Shah Alam, Selangor, Malaysia.',
-    skills: [
-      'problem-solving',
-      'time-management',
-      'decision-making',
-    ],
+    skills: ['problem-solving', 'time-management', 'decision-making'],
     rank: {
       title: 'Newbie',
-      votesCount: 0,
+      votesCount: 0
     },
     peopleHired: 0,
     events: [
@@ -470,7 +448,7 @@ export default {
         image: '/grab.jpg',
         desc:
           'A hackathon organized to unearth new solutions and ideas for traffic problems and road safety.',
-        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack'],
+        tags: ['safety', 'Grab', 'hackathon', 'MYTrafficHack']
       },
       {
         type: 'certificate',
@@ -478,7 +456,7 @@ export default {
         company: 'Certification of Project Management',
         position: 'CPM 2016',
         desc: 'Certification that involves testing on managing project skills.',
-        tags: ['project management', 'CPM 2016'],
+        tags: ['project management', 'CPM 2016']
       },
       {
         type: 'certificate',
@@ -488,7 +466,7 @@ export default {
         image: '/ceh.png',
         desc:
           'Master the advanced concepts on Ethical hacking such as corporate espionage, writing virus codes, exploit writing, and reverse engineering. You will understand advanced network packet analysis, securing IIS and Apache web servers, Windows system administration using PowerShell, Hacking SQL & Oracle database.',
-        tags: ['security', 'CEH'],
+        tags: ['security', 'CEH']
       },
       {
         type: 'education',
@@ -497,8 +475,8 @@ export default {
         company: 'Multimedia University',
         desc:
           'Degree in Information technology, specializing in Communication and Networking.',
-        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication'],
-      },
-    ],
-  },
+        tags: ['MMU', 'IT', 'Bachelor', 'Networking', 'Communication']
+      }
+    ]
+  }
 };
